8077f69b1e0e7b39608eb254539149ce26d44e6a
YukariChiba <i@0x7f.cc>
2022-04-22 16:05:54 +0800

    Fix bug

--- generate.sh
+++ generate.sh
@@ -2,13 +2,16 @@
 
 WEB_IP="172.20.168.252"
 
+echo "# Brain Upgrade List" > prevent-brain-upgrade.conf
+
 for file in ./blacklists/*.txt; do
   if [ -f "$file" ]; then
     while read p; 
     do
       if [[ "$p" != "#"* ]] && ! test -z "$p"; then
-        echo "address /$p/#6" >> prevent-brain-upgrade.conf
-        echo "address /$p/$WEB_IP" >> prevent-brain-upgrade.conf
+	pp=`echo $p | sed 's/\r//g'`
+        echo "address /$pp/#6" >> prevent-brain-upgrade.conf
+        echo "address /$pp/$WEB_IP" >> prevent-brain-upgrade.conf
       fi
     done <$file
   fi
